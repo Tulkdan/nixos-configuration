b24e556ff8801c2a34eebd12446dc1bee11f56c2
Pedro Correa <pedro8correa@gmail.com>
2023-05-04 00:24:22 -0300

    :memo: flakes on org mode

--- flake.nix
+++ flake.nix
@@ -1,21 +1,23 @@
 {
-  description = "Tulk'dan's system config";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+description = "Tulk'dan's system config";
+
+inputs = {
+  nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+};
+
+outputs = {self, nixpkgs}: let
+  system = "x86_64-linux";
+  hostname = "nixos";
+  pkgs = import nixpkgs {inherit system;};
+in {
+
+  nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
+    inherit system;
+
+    modules = [ ./system/configuration.nix ];
   };
 
-  outputs = {self, nixpkgs}: let
-    system = "x86_64-linux";
-    hostname = "nixos";
-    pkgs = import nixpkgs {inherit system;};
-  in {
+};
 
-    nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
-      inherit system;
-
-      modules = [ ./system/configuration.nix ];
-    };
-
-  };
 }
