3d3d1a77d467b1ac98013a6dd1f9eb71738fd52f
Pedro Correa <pedro8correa@gmail.com>
2023-05-03 22:58:53 -0300

    :sparkles: nix configuration flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Tulk'dan's system config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {self, nixpkgs}: let
+    system = "x86_64-linux";
+    hostname = "nixos";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+
+    nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
+      inherit system;
+
+      modules = [ ./system/configuration.nix ];
+    };
+
+  };
+}
---
+++ system/configuration.nix
@@ -0,0 +1,229 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, fetchFromGithub, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+    useOSProber = true;
+  };
+
+  networking = {
+    hostName = "nixos"; # Define your hostname.
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Enable networking
+    networkmanager.enable = true;
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
+
+  services = {
+
+    xserver = {
+      # Enable the X11 windowing system.
+      enable = true;
+
+      # Enable the GNOME Desktop Environment.
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+
+      # Configure keymap in X11
+      layout = "us";
+      xkbVariant = "";
+
+      updateDbusEnvironment = true;
+    };
+
+    # Enable CUPS to print documents.
+    printing.enable = true;
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
+
+    pcscd.enable = true;
+
+    # Enable the OpenSSH daemon.
+    openssh.enable = true;
+
+    # Adding support for flatpak
+    flatpak.enable = true;
+
+    emacs = {
+      enable = true;
+      package = pkgs.emacs;
+    };
+  };
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+
+  hardware.opengl.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  programs = {
+    # Enabling zsh
+    zsh = {
+      enable = true;
+      ohMyZsh = {
+        enable = true;
+        plugins = [ "git" ];
+      };
+    };
+
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+      enableExtraSocket = true;
+    };
+
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.pedro = {
+    isNormalUser = true;
+    description = "Pedro";
+    extraGroups = [ "networkmanager" "wheel" "tty" ];
+    packages = with pkgs; [
+      firefox
+    #  thunderbird
+      neovim
+      nodejs
+      ghc
+      alacritty
+      oh-my-zsh
+      direnv
+      gnupg
+      emacs
+      wezterm
+      vscodium
+
+      gnomeExtensions.pop-shell
+      gnomeExtensions.paperwm
+      just
+
+      eww-wayland
+      ripgrep
+      jq
+    ];
+    shell = pkgs.zsh;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment = {
+    gnome.excludePackages = (with pkgs; [
+      gnome-tour
+      gnome-text-editor
+    ]) ++ (with pkgs.gnome; [
+      gnome-music
+      gnome-shell
+      geary
+      epiphany
+      totem
+    ]);
+
+    systemPackages = with pkgs; [
+      git
+      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      wget
+    ];
+
+    variables = {
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+      BROWSER = "firefox";
+    };
+  };
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+
+  # List services that you want to enable:
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+
+
+  # Automatic GB
+  nix = {
+    settings = {
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+      experimental-features = ["nix-command" "flakes"];
+    };
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
+
+  # Installing fonts
+  fonts.fonts = with pkgs; [
+    material-design-icons
+    fira-code
+    hasklig
+  ];
+
+}
