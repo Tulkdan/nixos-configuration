3a002a1f87164ad53aa1344b531593bd813b1ce8
Pedro Correa <pedro8correa@gmail.com>
2023-05-04 00:35:24 -0300

    :memo: using readme to create flake

--- flake.nix
+++ flake.nix
@@ -1,23 +1,22 @@
 {
+  description = "Tulk'dan's system config";
 
-description = "Tulk'dan's system config";
-
-inputs = {
-  nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-};
-
-outputs = {self, nixpkgs}: let
-  system = "x86_64-linux";
-  hostname = "nixos";
-  pkgs = import nixpkgs {inherit system;};
-in {
-
-  nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
-    inherit system;
-
-    modules = [ ./system/configuration.nix ];
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-};
+  outputs = {self, nixpkgs}: let
+    system = "x86_64-linux";
+    hostname = "nixos";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+  
+    nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
+      inherit system;
+  
+      modules = [ ./system/configuration.nix ];
+    };
+  
+  };
 
 }
