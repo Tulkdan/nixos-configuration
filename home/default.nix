36fa8df37bc819ecfe6d2eac6e2280385a8333cb
Pedro Correa <pedro8correa@gmail.com>
2023-05-30 20:49:35 -0300

    :sparkles: gtk configuration

--- home/default.nix
+++ home/default.nix
@@ -126,4 +126,15 @@
   };
 
   xdg.enable = true;
+
+  gtk = {
+    enable = true;
+
+    font = {
+      name = "Fira Code Regular";
+      size = 11;
+    };
+
+    cursorTheme.name = "Bibata-Modern-Classic";
+  };
 }
