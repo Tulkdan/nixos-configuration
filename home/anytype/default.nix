0f64e23ec4637d4304eab4f3e484399c81c1d257
Pedro Correa <pedro.correa@zigpay.com.br>
2023-07-11 10:35:13 -0300

    :art: formatting nix files

--- home/anytype/default.nix
+++ home/anytype/default.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-let
+{pkgs, ...}: let
   version = "0.32.3";
   anytype = pkgs.appimageTools.wrapType2 {
     name = "Anytype";
@@ -8,9 +7,8 @@ let
       url = "https://anytype-release.fra1.cdn.digitaloceanspaces.com/Anytype-0.32.3.AppImage";
       sha256 = "sha256-usRGwQuYNETQu+a23UeW384tOFXLfsjO6Tc+57utCvk=";
     };
-    extraPkgs = pkgs: with pkgs; [ libsecret xdg-desktop-portal hicolor-icon-theme ];
+    extraPkgs = pkgs: with pkgs; [libsecret xdg-desktop-portal hicolor-icon-theme];
   };
-in
-{
-  home.packages = [ anytype ];
+in {
+  home.packages = [anytype];
 }
