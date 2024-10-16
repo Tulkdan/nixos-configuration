c7164aa2955fa1c5e3182ca453f51ce4908494b4
Pedro Correa <pedro8correa@gmail.com>
2024-10-16 00:20:20 -0300

    :sparkles: (Nvim) conform to format code

---
+++ home/neovim/lua/plugins/conform.lua
@@ -0,0 +1,14 @@
+return {
+  'stevearc/conform.nvim',
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      format_on_save = {
+        lsp_format = "fallback",
+        timeout_ms = 500,
+      },
+    })
+  end
+}
