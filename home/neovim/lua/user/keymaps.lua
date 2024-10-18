399b2e1e5b85baf172c0120494448c9466089ecd
Pedro Correa <pedro8correa@gmail.com>
2024-10-18 09:29:00 -0300

    :sparkles: (Nvim) keymap for diagnostics

--- home/neovim/lua/user/keymaps.lua
+++ home/neovim/lua/user/keymaps.lua
@@ -17,3 +17,21 @@ vim.keymap.set('n', '<left>', '<cmd>vertical resize -5<cr>')
 vim.keymap.set('n', '<up>', '<cmd>resize -5<cr>')
 vim.keymap.set('n', '<down>', '<cmd>resize +5<cr>')
 vim.keymap.set('n', '<right>', '<cmd>vertical resize +5<cr>')
+
+-- Diagnostic
+-- Turn off inline diagnostics
+vim.diagnostic.config({
+    virtual_text = false,
+})
+
+-- Show all diagnostic on current line in floating window
+vim.api.nvim_set_keymap(
+  'n', '<Leader>d', ':lua vim.diagnostic.open_float()<CR>',
+  { noremap = true, silent = true }
+)
+
+-- Go to next diagnostic
+vim.api.nvim_set_keymap(
+  'n', '<Leader>n', ':lua vim.diagnostic.goto_next()<CR>',
+  { noremap = true, silent = true }
+)
