3d3ac8aafe5e29f01de4c9f29fa2589e47b2f708
Pedro Correa <pedro8correa@gmail.com>
2025-02-26 14:50:15 -0300

    :sparkles: [Git] default configs

--- home/git/default.nix
+++ home/git/default.nix
@@ -4,10 +4,33 @@
     userName = "Pedro Correa";
     ignores = ["*~" "*.swp"];
     extraConfig = {
-      commit.template = "~/.config/git/message";
+      commit = {
+        template = "~/.config/git/message";
+        verbose = true;
+      };
       rerere = {
         enable = true;
         autoUpdate = true;
+      };
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+      init.defaultBranch = "main";
+      merge.conflictstyle = "zdiff3";
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      push = {
+        autoSetupRemote = true;
+        followTags = true;
+      };
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
       };
     };
 
