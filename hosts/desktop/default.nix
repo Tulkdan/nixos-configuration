ce7762f56d7c4524a57bb5163087609ff3453b83
Pedro Correa <pedro8correa@gmail.com>
2025-05-26 18:13:36 -0300

    :sparkles: Host config obs studio virtual camera

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -202,4 +202,13 @@
   ];
 
   virtualisation.docker.enable = true;
+
+  # https://nixos.wiki/wiki/OBS_Studio
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
 }
