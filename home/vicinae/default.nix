5c2272badc9149c0c7e43de41e76a1cbf85f699d
Pedro Correa <pedro8correa@gmail.com>
2025-10-05 22:38:30 -0300

    :sparkles: configurin vicinae

--- home/vicinae/default.nix
+++ home/vicinae/default.nix
@@ -2,5 +2,9 @@
   services.vicinae = {
     enable = true;
     autoStart = true;
+    settings = {
+      font.normal = "Monaspace Krypton";
+      theme.name = "catppuccin-mocha.json";
+    };
   };
 }
