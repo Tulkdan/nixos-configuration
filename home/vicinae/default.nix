03ff15602bfdfc9871503271d2b4d6352b3677c5
Pedro Correa <pedro8correa@gmail.com>
2026-01-05 22:25:27 -0300

    :arrow_up: updated flake

--- home/vicinae/default.nix
+++ home/vicinae/default.nix
@@ -1,7 +1,10 @@
 {
   services.vicinae = {
     enable = true;
-    autoStart = true;
+    systemd = {
+      enable = true;
+      autoStart = true;
+    };
     settings = {
       font.normal = "Monaspace Krypton";
       theme.name = "catppuccin-mocha.json";
