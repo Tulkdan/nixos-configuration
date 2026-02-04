df130ddfa701b6bf51dcdb3422778891acaac206
Pedro Correa <pedro8correa@gmail.com>
2026-02-03 23:19:41 -0300

    :sparkles: (Vicinae) updated configuration

--- home/vicinae/default.nix
+++ home/vicinae/default.nix
@@ -4,10 +4,18 @@
     systemd = {
       enable = true;
       autoStart = true;
+      environment = {
+        USE_LAYER_SHELL = 1;
+      };
     };
     settings = {
+      close_on_focus_loss = true;
       font.normal = "Monaspace Krypton";
-      theme.name = "catppuccin-mocha.json";
+      theme = {
+        dark.name = "vicinae-dark";
+        light.name = "vicinae-dark";
+      };
+      launcher_window.opacity = 1;
     };
   };
 }
