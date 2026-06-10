d63e2a03181bde393dc950c02ffee1971bc6c349
Pedro Correa <pedro8correa@gmail.com>
2026-06-09 22:53:12 -0300

    :sparkles: niri configuration

--- home/noctalia/default.nix
+++ home/noctalia/default.nix
@@ -4,59 +4,66 @@
   inputs,
   ...
 }: {
-    imports = [
-      inputs.noctalia.homeModules.default
-    ];
+  imports = [
+    inputs.noctalia.homeModules.default
+  ];
 
-      # configure options
-    programs.noctalia-shell = {
-      enable = true;
-      settings = {
-        # configure noctalia here
-        bar = {
-          density = "compact";
-          position = "top";
-          showCapsule = false;
-          widgets = {
-            left = [
+  # configure options
+  programs.noctalia-shell = {
+    enable = true;
+    settings = {
+      # configure noctalia here
+      bar = {
+        barType = "floating";
+        density = "compact";
+        position = "top";
+        showCapsule = false;
+        marginVertical = 6;
+        marginHorizontal = 6;
+        widgets = {
+          left = [
             {
-                hideUnoccupied = false;
-                id = "Workspace";
-                labelMode = "none";
-              }
-            ];
-            center = [
-              {
-                formatHorizontal = "HH:mm";
-                formatVertical = "HH mm";
-                id = "Clock";
-                useMonospacedFont = true;
-                usePrimaryColor = true;
-              }
-            ];
-            right = [
-              { id = "Volume"; }
-              {
-                id = "Bluetooth";
-              }
-              {
-                id = "Network";
-              }
-              {
-                id = "ControlCenter";
-                useDistroLogo = true;
-              }
-            ];
-          };
-        };
-        colorSchemes.predefinedScheme = "Eldritch";
-        general = {
-          avatarImage = "/home/drfoobar/.face";
-          radiusRatio = 0.2;
-        };
-        location = {
-          name = "Sao Paulo, Brazil";
+              hideUnoccupied = false;
+              id = "Workspace";
+              labelMode = "none";
+            }
+          ];
+          center = [
+            {
+              formatHorizontal = "HH:mm";
+              formatVertical = "HH mm";
+              id = "Clock";
+              useMonospacedFont = true;
+              usePrimaryColor = true;
+            }
+          ];
+          right = [
+            {id = "Volume";}
+            {
+              id = "Bluetooth";
+            }
+            {
+              id = "Network";
+            }
+            {
+              id = "ControlCenter";
+              useDistroLogo = true;
+            }
+          ];
         };
       };
+      colorSchemes.predefinedScheme = "Eldritch";
+      general = {
+        radiusRatio = 0.2;
+      };
+      location = {
+        name = "Sao Paulo, Brazil";
+      };
+
+      ui = {
+        fontDefault = "Monaspace Krypton";
+        fontFixed = "Monaspace Krypton";
+      };
     };
+  };
 }
